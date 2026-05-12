6a8bc4af60fe3c237fecb9ba438c942b207d0c6d
Ralph <rdugue1@gmail.com>
2026-05-12 15:34:03 -0400

    Synching

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,16 +6,18 @@ local config = wezterm.config_builder()
 
 config.wsl_domains = {
     {
-        name = 'Ubuntu',
+        name = 'WSL:Ubuntu',
         distribution = 'Ubuntu',
     },
 }
 
-config.font = wezterm.font('JetBrains Mono', { weight = "Bold" })
+config.default_domain = 'WSL:Ubuntu'
+
+config.font = wezterm.font('JetBrainsMono NF', { weight = "Bold" })
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.4
-config.font_size = 12
+config.font_size = 14
 config.color_scheme = 'Matrix (terminal.sexy)'
 
 -- Finally, return the configuration to wezterm:
